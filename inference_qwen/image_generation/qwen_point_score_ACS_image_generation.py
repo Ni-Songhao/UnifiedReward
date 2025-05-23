ea39e4139c5bf79a1b5a7bcabdb38f6d7211d112
SII-Yibin Wang (王逸彬) <95867770+CodeGoat24@users.noreply.github.com>
2025-05-23 13:10:08 +0800

    Update qwen_point_score_ACS_image_generation.py

--- inference_qwen/image_generation/qwen_point_score_ACS_image_generation.py
+++ inference_qwen/image_generation/qwen_point_score_ACS_image_generation.py
@@ -63,7 +63,7 @@ inputs = processor(
 
 
 with torch.no_grad():
-    generated_ids = model.generate(**inputs, max_new_tokens=512)
+    generated_ids = model.generate(**inputs, max_new_tokens=4096)
 generated_ids_trimmed = [
     out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
 ]
