0d578c6735681eda06fe3fc41c7befaed8bb59a3
SII-Yibin Wang (王逸彬) <95867770+CodeGoat24@users.noreply.github.com>
2025-06-11 21:48:57 +0800

    Update vllm_server.sh

--- UnifiedReward-Think/vllm_qwen/vllm_server.sh
+++ UnifiedReward-Think/vllm_qwen/vllm_server.sh
@@ -2,8 +2,8 @@ vllm serve CodeGoat24/UnifiedReward-Think-qwen-7b \
     --host /ip/address \
     --trust-remote-code \
     --served-model-name UnifiedReward \
-    --gpu-memory-utilization 0.7 \
+    --gpu-memory-utilization 0.9 \
     --tensor-parallel-size 4 \
-    --pipeline-parallel-size 2 \
+    --pipeline-parallel-size 1 \
     --limit-mm-per-prompt image=2 \
     --port 8080
