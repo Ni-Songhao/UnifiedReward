621dea14e7751ce04874e7115db5ef060f816e40
SII-Yibin Wang (王逸彬) <95867770+CodeGoat24@users.noreply.github.com>
2025-06-18 19:39:27 +0800

    Update qwen_genAI_bench_image_test.py

--- benchmark_evaluation/GenAI-Bench-Image/qwen_genAI_bench_image_test.py
+++ benchmark_evaluation/GenAI-Bench-Image/qwen_genAI_bench_image_test.py
@@ -1,13 +1,12 @@
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
-from datasets import load_from_disk
+from datasets import load_dataset, load_from_disk
 from PIL import Image
 import torch
 import tqdm
 import os
 import random
 import json
-
 
 model_path = 'CodeGoat24/UnifiedReward-qwen-7b'
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
